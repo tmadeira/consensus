2cce7cef65eb4645f935a7be83d61264184c7d49
Tiago Madeira <tmadeira@gmail.com>
2020-08-26 19:01:55 -0300

    Use N = [25, 49, 81] for experiment 2020-08-26.

--- src/2020-08-26.py
+++ src/2020-08-26.py
@@ -3,7 +3,7 @@ Experiment: Given 4 structures, test memory distribution to generate a chart
 for the paper.
 
 Setup:
-- n = 81, times = 1000
+- n_1 = 25, n_2 = 49, n_3 81, times = 1000
 - 4 graphs (cycle, complete graph, gridcycle, expanded cycle)
 - uniform random initial vectors
 - 50 tests for each graph, with memory uniformly ranging from [0.01, 0.99] to [1.00, 0.00]
@@ -14,8 +14,8 @@ import numpy as np
 
 import simulation
 
-P = 6
-n = 81
+P = 12
+N = [25, 49, 81]
 buckets = 50
 times = 1000
 
@@ -29,15 +29,16 @@ tps = [
 # Create tasks.
 tasks = []
 seed = 0
-for p in np.linspace(0.1, 1.00, num=buckets):
-    for tp in tps:
-        tasks.append({
-          'seed': seed,
-          'mem': [p, 1-p],
-          'n': n,
-          'tp': tp,
-        })
-        seed += 1
+for n in N:
+    for p in np.linspace(0.1, 1.00, num=buckets):
+        for tp in tps:
+            tasks.append({
+              'seed': seed,
+              'mem': [p, 1-p],
+              'n': n,
+              'tp': tp,
+            })
+            seed += 1
 
 def run_task(task):
     return simulation.run(task['n'],
@@ -52,4 +53,4 @@ p = Pool(P)
 results = p.map(run_task, tasks)
 
 for i in range(len(tasks)):
-  print(tasks[i]['tp'], tasks[i]['mem'][0], results[i][0], results[i][1], results[i][2])
+  print(tasks[i]['tp'], tasks[i]['n'], tasks[i]['mem'][0], results[i][0], results[i][1], results[i][2])
