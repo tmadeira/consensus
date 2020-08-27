b71477c267e9b5cd8decdd3aaa307b443a9498e4
Tiago Madeira <tmadeira@gmail.com>
2020-08-27 18:46:08 -0300

    Fix 2020-08-26 for macOS.

--- src/2020-08-26.py
+++ src/2020-08-26.py
@@ -14,28 +14,19 @@ import numpy as np
 
 import simulation
 
+# number of cores
 P = 12
+
+# remaining parameters
 N = [25, 49, 81]
 buckets = 50
 times = 1000
-
 tps = [
     'cycle',
     'complete',
     'gridcycle',
     'expandedcycle',
 ]
-
-# Create tasks.
-tasks = []
-for n in N:
-    for p in np.linspace(0.1, 1.00, num=buckets):
-        for tp in tps:
-            tasks.append({
-              'mem': [p, 1-p],
-              'n': n,
-              'tp': tp,
-            })
 
 def run_task(task):
     return simulation.run(task['n'],
@@ -46,8 +37,23 @@ def run_task(task):
         rand_init=True,
     )
 
-p = Pool(P)
-results = p.map(run_task, tasks)
+def run():
+    # Create tasks.
+    tasks = []
+    for n in N:
+        for p in np.linspace(0.1, 1.00, num=buckets):
+            for tp in tps:
+                tasks.append({
+                  'mem': [p, 1-p],
+                  'n': n,
+                  'tp': tp,
+                })
 
-for i in range(len(tasks)):
-  print(tasks[i]['tp'], tasks[i]['n'], tasks[i]['mem'][0], results[i][0], results[i][1], results[i][2])
+    p = Pool(P)
+    results = p.map(run_task, tasks)
+
+    for i in range(len(tasks)):
+      print(tasks[i]['tp'], tasks[i]['n'], tasks[i]['mem'][0], results[i][0], results[i][1], results[i][2])
+
+if __name__ == '__main__':
+    run()
