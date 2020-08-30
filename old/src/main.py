b9a60913ecb87463fd100b9edf3eb76a1fb9401c
Tiago Madeira <tmadeira@gmail.com>
2020-08-29 22:35:44 -0300

    Remove legacy python code (old directory).

--- old/src/main.py
+++
@@ -1,7 +0,0 @@
-import simulation
-
-for n in [3, 5, 7, 11, 17]:
-    for tp in ['cycle', 'complete']:
-        for mem in [[1], [0.8, 0.2], [0.6, 0.3, 0.1]]:
-            print(n, tp, mem, simulation.run(n, tp=tp, memory=mem))
-
