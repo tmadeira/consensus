25e61c98e446a39c93f9dc4e1d73f9097b0bef26
Tiago Madeira <tmadeira@gmail.com>
2020-09-01 08:03:20 -0300

    Improve fig2, generate fig2-clique.csv.

--- fig2.py
+++ fig2.py
@@ -4,24 +4,24 @@ import numpy as np
 import os
 import time
 
-times = 1000
-tp = 'torus'
+times = 100000
 
 threads = multiprocessing.cpu_count()
 
-start = time.time()
+for tp in ['cycle', 'clique', 'torus']:
+    start = time.time()
 
-with open('fig2.csv', 'w') as f:
-    writer = csv.writer(f)
-    for m in range(3, 52, 2):
-        n = m*m
-        for p in [0.8, 1]:
-            stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
-            results = list(map(int, stream.read().strip().split('\n')))
-            print(time.time() - start, 'seconds elapsed.')
-            print('finished', tp, n, p)
-            print('avg:', np.mean(results))
-            print('stdev:', np.std(results))
-            print('median:', np.median(results))
-            print('')
-            writer.writerow([tp, n, p, np.mean(results), np.std(results), np.mean(results)] + results)
+    with open('fig2-%s.csv' % tp, 'w') as f:
+        writer = csv.writer(f)
+        for m in range(3, 52, 2):
+            n = m*m
+            for p in [0.8, 0.9, 1]:
+                stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
+                results = list(map(int, stream.read().strip().split('\n')))
+                print(time.time() - start, 'seconds elapsed.')
+                print('finished', tp, n, p)
+                print('avg:', np.mean(results))
+                print('stdev:', np.std(results))
+                print('median:', np.median(results))
+                print('')
+                writer.writerow([tp, n, p, np.mean(results), np.std(results), np.mean(results)] + results)
