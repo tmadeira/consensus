5b26158ba72087d87ff2855c35bbf17c72b3f12d
Tiago Madeira <tmadeira@gmail.com>
2020-08-31 21:47:56 -0300

    Use dynamic allocation for queue tasks (remove QUEUE_MAX constant).

--- src/queue.cpp
+++ src/queue.cpp
@@ -6,10 +6,18 @@
 #include "queue.h"
 #include "task.h"
 
-void queue_init(queue_t *Q) {
+void queue_init(queue_t *Q, int length) {
   pthread_mutex_init(&Q->mutex, NULL);
   pthread_cond_init(&Q->has_something, NULL);
+
+  Q->tasks = (task_t *) malloc(sizeof(task_t) * length);
+  if (!Q->tasks) {
+    fprintf(stderr, "could not allocate queue\n");
+    exit(1);
+  }
+
   Q->start = Q->end = 0;
+  Q->length = length;
 }
 
 void queue_destroy(queue_t *Q) {
@@ -20,7 +28,7 @@ void queue_destroy(queue_t *Q) {
 void queue_push(queue_t *Q, task_t task) {
   pthread_mutex_lock(&Q->mutex);
   Q->tasks[Q->end++] = task;
-  Q->end %= QUEUE_MAX;
+  Q->end %= Q->length;
   pthread_cond_signal(&Q->has_something);
   pthread_mutex_unlock(&Q->mutex);
 }
@@ -32,7 +40,7 @@ task_t queue_pop(queue_t *Q) {
   }
 
   task_t task = Q->tasks[Q->start++];
-  Q->start %= QUEUE_MAX;
+  Q->start %= Q->length;
   pthread_mutex_unlock(&Q->mutex);
 
   return task;
--- src/queue.h
+++ src/queue.h
@@ -5,17 +5,16 @@
 
 #include "task.h"
 
-#define QUEUE_MAX 100000
-
 typedef struct {
-  task_t tasks[QUEUE_MAX];
+  task_t *tasks;
+  int length;
   int start;
   int end;
   pthread_mutex_t mutex;
   pthread_cond_t has_something;
 } queue_t;
 
-void queue_init(queue_t *Q);
+void queue_init(queue_t *Q, int length);
 void queue_destroy(queue_t *Q);
 void queue_push(queue_t *Q, task_t task);
 task_t queue_pop(queue_t *Q);
