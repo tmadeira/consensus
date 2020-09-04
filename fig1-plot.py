3fc9b20f1689f0f38c2fcd202baf7b9f2eb827f0
Tiago Madeira <tmadeira@gmail.com>
2020-09-04 18:31:31 -0300

    Add path to fig1-plot.

--- fig1-plot.py
+++ fig1-plot.py
@@ -17,15 +17,18 @@ with open('fig1.csv') as f:
 x = []
 for i in np.linspace(0.1, 1.0, num=buckets):
     x.append(i)
+x = x[:-1]
 
 for tp in data:
     for i in range(len(data[tp])):
         data[tp][i] = data[tp][i] / data[tp][-1]
+    data[tp] = data[tp][:-1]
 
-plt.plot(x, np.ones(buckets), 'k')
+plt.plot(x, np.ones(buckets-1), 'k')
 plt.plot(x, data['cycle'], 'r')
 plt.plot(x, data['torus'], 'g')
 plt.plot(x, data['clique'], 'b')
-plt.legend(('no memory', 'cycle', 'torus', 'clique'), loc='upper right')
+plt.plot(x, data['path'], 'y')
+plt.legend(('no memory', 'cycle', 'torus', 'clique', 'path'), loc='upper right')
 plt.title('n = %s' % n)
 plt.show()
