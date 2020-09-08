f708057ac37ff7ab3dcb7afe2b0c4d6f81b49672
Tiago Madeira <tmadeira@gmail.com>
2020-09-07 21:23:55 -0300

    Improve fig2 generation.

--- fig2.py
+++ fig2.py
@@ -5,75 +5,33 @@ import os
 import time
 
 times = 10000
-
 threads = multiprocessing.cpu_count()
 
+def run(tp, n, writer):
+    start = time.time()
+    for p in [0.9, 1]:
+        stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
+        results = list(map(int, stream.read().strip().split('\n')))
+        print(time.time() - start, 'seconds elapsed.')
+        print('finished', tp, n, p)
+        print('avg:', np.mean(results))
+        print('stdev:', np.std(results))
+        print('median:', np.median(results))
+        print('')
+        writer.writerow([tp, n, p, np.mean(results), np.std(results), np.median(results)] + results)
+
+# Powers of two (9) - n in range [7, 2047].
 for tp in ['bintree']:
-    start = time.time()
-
     with open('fig2-%s.csv' % tp, 'w') as f:
         writer = csv.writer(f)
-        for m in range(2, 11):
+        for m in range(3, 12):
             n = 2**m-1
-            for p in [0.9, 1]:
-                stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
-                results = list(map(int, stream.read().strip().split('\n')))
-                print(time.time() - start, 'seconds elapsed.')
-                print('finished', tp, n, p)
-                print('avg:', np.mean(results))
-                print('stdev:', np.std(results))
-                print('median:', np.median(results))
-                print('')
-                writer.writerow([tp, n, p, np.mean(results), np.std(results), np.median(results)] + results)
+            run(tp, n, writer)
 
-for tp in ['torus']:
-    start = time.time()
-
+# Odd perfect squares - n in range [9, 2025].
+for tp in ['biclique', 'clique', 'cycle', 'path', 'torus']:
     with open('fig2-%s.csv' % tp, 'w') as f:
         writer = csv.writer(f)
-        for m in range(3, 40, 2):
+        for m in range(3, 46, 2):
             n = m*m
-            for p in [0.9, 1]:
-                stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
-                results = list(map(int, stream.read().strip().split('\n')))
-                print(time.time() - start, 'seconds elapsed.')
-                print('finished', tp, n, p)
-                print('avg:', np.mean(results))
-                print('stdev:', np.std(results))
-                print('median:', np.median(results))
-                print('')
-                writer.writerow([tp, n, p, np.mean(results), np.std(results), np.median(results)] + results)
-
-for tp in ['biclique']:
-    start = time.time()
-
-    with open('fig2-%s.csv' % tp, 'w') as f:
-        writer = csv.writer(f)
-        for n in range(4, 1500, 64):
-            for p in [0.9, 1]:
-                stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
-                results = list(map(int, stream.read().strip().split('\n')))
-                print(time.time() - start, 'seconds elapsed.')
-                print('finished', tp, n, p)
-                print('avg:', np.mean(results))
-                print('stdev:', np.std(results))
-                print('median:', np.median(results))
-                print('')
-                writer.writerow([tp, n, p, np.mean(results), np.std(results), np.median(results)] + results)
-
-for tp in ['clique', 'cycle', 'path']:
-    start = time.time()
-
-    with open('fig2-%s.csv' % tp, 'w') as f:
-        writer = csv.writer(f)
-        for n in range(3, 1500, 64):
-            for p in [0.9, 1]:
-                stream = os.popen('./simulator %s %d %d %f %d' % (tp, n, times, p, threads))
-                results = list(map(int, stream.read().strip().split('\n')))
-                print(time.time() - start, 'seconds elapsed.')
-                print('finished', tp, n, p)
-                print('avg:', np.mean(results))
-                print('stdev:', np.std(results))
-                print('median:', np.median(results))
-                print('')
-                writer.writerow([tp, n, p, np.mean(results), np.std(results), np.median(results)] + results)
+            run(tp, n, writer)
