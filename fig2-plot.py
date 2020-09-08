38af2e3e771989e5a95b509b0307d4cbac1d3721
Tiago Madeira <tmadeira@gmail.com>
2020-09-07 21:23:41 -0300

    Improve fig2 plot.

--- fig2-plot.py
+++ fig2-plot.py
@@ -3,40 +3,33 @@ import gzip
 import matplotlib.pyplot as plt
 import numpy as np
 
-for tp in ['clique', 'torus', 'cycle', 'path']:
-    for measure in ['mean', 'median']:
-        data = {}
+colors = 'rgbcmy'
+col = 0
+
+tps = ['biclique', 'bintree', 'clique', 'cycle', 'torus', 'path']
+
+plt.plot(list(range(0, 1600)), [1] * 1600, 'k')
+
+for tp in tps:
+    with gzip.open('fig2-%s.csv.gz' % tp, 'rt') as f:
+        c = csv.reader(f)
         x = []
+        y = []
+        for row in c:
+            [n, p, mean] = row[1:4]
+            n = int(n)
+            p = float(p)
+            mean = float(mean)
+            if p == 0.9:
+                x.append(n)
+                y.append(mean)
+            elif p == 1:
+                y[len(y)-1] /= mean
 
-        with gzip.open('fig2-%s.csv.gz' % tp, 'rt') as f:
-            c = csv.reader(f)
-            for row in c:
-                [n, p, mean, _, median] = row[1:6]
-                n = int(n)
-                p = float(p)
-                mean = float(mean)
-                median = float(median)
-                if p not in data:
-                    data[p] = []
-                if p == 1:
-                    x.append(n)
-                if measure == 'mean':
-                    data[p].append(mean)
-                else:
-                    data[p].append(median)
+        plt.plot(x, y, colors[col])
+        col += 1
 
-        for p in data:
-            if p == 1:
-                continue
-            for i in range(len(data[p])):
-                data[p][i] = data[p][i] / data[1][i]
+plt.legend(['baseline'] + tps, loc='upper right')
+plt.title('fig. 2 (p=0.9, times=10000)')
+plt.show()
 
-        for i in range(len(data[1])):
-            data[1][i] = 1
-
-        plt.plot(x, data[1.0], 'k')
-        plt.plot(x, data[0.9], 'b')
-        plt.plot(x, data[0.8], 'r')
-        plt.legend(('p=1', 'p=0.9', 'p=0.8'), loc='upper right')
-        plt.title('%s (%s)' % (tp, measure))
-        plt.show()
