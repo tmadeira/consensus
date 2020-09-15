dc5018abee0bb80e392787d585fd7806977dcea4
Tiago Madeira <tmadeira@gmail.com>
2020-09-15 16:05:08 -0300

    Add code to generate median plots in TIKZ/PGF.

--- exp1-pgf.py
+++ exp1-pgf.py
@@ -1,18 +1,26 @@
 import csv
 import gzip
 import numpy as np
+import sys
 
-def run():
+def run(measure):
+    if measure != 'mean' and measure != 'median':
+        print('<measure> must be "mean" or "median"')
+        quit()
+
     buckets = 30
     data = {}
 
     with gzip.open('data/exp1.csv.gz', 'rt') as f:
         c = csv.reader(f)
         for row in c:
-            [tp, n, p, mean] = row[:4]
+            [tp, n, p, mean, _, median] = row[:6]
             if tp not in data:
                 data[tp] = []
-            data[tp].append(float(mean))
+            if measure == 'median':
+                data[tp].append(float(median))
+            else:
+                data[tp].append(float(mean))
 
     x = []
     for i in np.linspace(0.1, 1.0, num=buckets):
@@ -35,4 +43,7 @@ def run():
     print("\\legend{" + ",".join(legend) + "}")
 
 if __name__ == '__main__':
-    run()
+    measure = 'mean'
+    if len(sys.argv) > 1:
+        measure = sys.argv[1]
+    run(measure)
--- exp2-pgf.py
+++ exp2-pgf.py
@@ -1,8 +1,12 @@
 import csv
-import numpy as np
 import gzip
+import numpy as np
+import sys
 
-def run():
+def run(measure):
+    if measure != 'mean' and measure != 'median':
+        print('<measure> must be "mean" or "median"')
+        quit()
     tps = ['biclique', 'bintree', 'clique', 'cycle', 'torus']
 
     legend = []
@@ -12,15 +16,22 @@ def run():
             x = []
             y = []
             for row in c:
-                [n, p, mean] = row[1:4]
+                [n, p, mean, _, median] = row[1:6]
                 n = int(n)
                 p = float(p)
                 mean = float(mean)
+                median = float(median)
+
+                if measure == 'mean':
+                    val = mean
+                else:
+                    val = median
+
                 if p == 0.9:
                     x.append(n)
-                    y.append(mean)
+                    y.append(val)
                 elif p == 1:
-                    y[len(y)-1] /= mean
+                    y[len(y)-1] /= val
 
             print("\\addplot coordinates {")
             for i in range(len(y)):
@@ -32,4 +43,7 @@ def run():
     print("\\legend{" + ",".join(legend) + "}")
 
 if __name__ == '__main__':
-    run()
+    measure = 'mean'
+    if len(sys.argv) > 1:
+        measure = sys.argv[1]
+    run(measure)
